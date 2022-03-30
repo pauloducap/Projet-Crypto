b8fe5b72cb8f5520e960fe229794feaa0e0d1dd7
Lucas999999 <lucas.fallous24@gmail.com>
2022-03-30 17:49:20 +0200

    :sparkles: Bot discord

--- src/bot/bot-discord.py
+++ src/bot/bot-discord.py
@@ -1,11 +1,31 @@
+import os
 import discord
+from dotenv import load_dotenv
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+load_dotenv()
 
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+client = discord.Client()
 
-client = MyClient()
-client.run('Discord token')
+@client.event
+async def on_ready():
+    print("le bot est prêt.")
+
+@client.event
+async def on_message(message):
+    if message.content.lower() == "test":
+        await message.channel.send("noob", delete_after=10)
+    if message.content.startswith("!delete"):
+        nbr = int(message.content.split()[1])
+        messageList = await message.channel.history(limit=nbr+1).flatten()
+        
+        for delMessage in messageList:
+            await delMessage.delete()
+    
+client.run(os.getenv("TOKEN"))
+
+# class MyClient(discord.Client):
+#     async def on_ready(self):
+#         print('Logged on as {0}!'.format(self.user))
+
+#     async def on_message(self, message):
+#         print('Message from {0.author}: {0.content}'.format(message))
